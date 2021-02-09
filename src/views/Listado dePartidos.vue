f652271e433cd67a16682c0e9d4231b63a2898a0
carofrias <c.frias.torrejon@gmail.com>
2021-02-09 16:08:15 -0300

    Maps agregados

---
+++ src/views/Listado dePartidos.vue
@@ -0,0 +1,16 @@
+<template>
+    <ListadoPartidos /> <!-- declaramos que vamos a trabajar con el componente recién creado "ListadoPartidos" -->
+</template>
+
+<script>
+import ListadoPartidos from "@/components/ListadoPartidos.Vue";
+export default {
+    components: {
+        ListadoPartidos
+    }
+}
+</script>
+
+<style>
+
+</style>
